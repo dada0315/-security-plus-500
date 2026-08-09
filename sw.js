75cafba7c847402e7f8a14e09b25c507c69e21b7
dada0315 <smkr3758@icloud.com>
2026-08-09 20:29:44 +0900

    Update service worker to version 4 with enhancements

--- sw.js
+++ sw.js
@@ -1,1 +1,71 @@
-const CACHE='secplus500-v3';self.addEventListener('install',e=>e.waitUntil(caches.open(CACHE).then(c=>c.addAll(['./','./index.html','./questions.json','./manifest.json']))));self.addEventListener('activate',e=>e.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k))))));self.addEventListener('fetch',e=>e.respondWith(caches.match(e.request).then(r=>r||fetch(e.request).then(x=>{const c=x.clone();caches.open(CACHE).then(k=>k.put(e.request,c));return x}))))
+const CACHE = 'secplus500-v4';
+
+self.addEventListener('install', event => {
+
+  event.waitUntil(
+
+    caches.open(CACHE).then(cache =>
+
+      cache.addAll([
+
+        './',
+
+        './index.html',
+
+        './questions.json',
+
+        './manifest.json'
+
+      ])
+
+    )
+
+  );
+
+  self.skipWaiting();
+
+});
+
+self.addEventListener('activate', event => {
+
+  event.waitUntil(
+
+    caches.keys().then(keys =>
+
+      Promise.all(
+
+        keys
+
+          .filter(key => key !== CACHE)
+
+          .map(key => caches.delete(key))
+
+      )
+
+    ).then(() => self.clients.claim())
+
+  );
+
+});
+
+self.addEventListener('fetch', event => {
+
+  event.respondWith(
+
+    fetch(event.request)
+
+      .then(response => {
+
+        const copy = response.clone();
+
+        caches.open(CACHE).then(cache => {
+
+          cache.put(event.request, copy);
+
+        });
+
+        return response;
+
+      })
+
+      .c
